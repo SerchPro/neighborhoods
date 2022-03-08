fc00c981ab0e6ca0893c7f31c42facd67cb86cba
SerchPro <proyects_serch@hotmail.com>
2022-03-08 17:47:30 -0600

    first commit

---
+++ src/NeighborhoodApp.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types'
+
+const NeighborhoodApp = props => {
+  return (
+    <div>
+        <h1> Hola mundo</h1>
+    </div>
+  )
+}
+
+NeighborhoodApp.propTypes = {}
+
+export default NeighborhoodApp
