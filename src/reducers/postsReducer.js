08e316508f4e5640d702a1f5b0c06ed0d6e2be82
SerchPro <proyects_serch@hotmail.com>
2022-03-09 17:57:06 -0600

    adding post reducer

---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,13 @@
+
+
+const initialState = {
+    posts:[],
+    active: null
+}
+
+export const  postsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { authReducer } from "./authReducer";
+import { postsReducer } from "./postsReducer";
 
 
 
 export const rootReducer = combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    posts: postsReducer
 })
