ce3483c3aa9d34d27b8e729ffbc42f068ba60709
SerchPro <proyects_serch@hotmail.com>
2022-03-09 17:01:54 -0600

    adding index

---
+++ src/types/index.js
@@ -0,0 +1,9 @@
+export const types = {
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start Register',
+    authStartStarTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
+
+}
