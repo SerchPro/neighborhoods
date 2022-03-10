0512725a6c53fc8559fc566fa305b0b2cc810f9c
SerchPro <proyects_serch@hotmail.com>
2022-03-10 01:55:04 -0600

    correction to className

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -23,14 +23,14 @@ const Post = ({image, user, time, message}) => {
 
         <div className="actions">
           {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
+          <i className="far fa-comment"></i>
+          <i className="fas fa-retweet"></i>
+          <i className="far fa-heart"></i>
+          <i className="fas fa-share"></i>
         </div>
       </div>
 
-      <i class="fas fa-ellipsis-h"></i>
+      <i className="fas fa-ellipsis-h"></i>
     </div>
   )
 }
