0c906785a1024eb4496505da04bbb5b8ba5a7c4c
SerchPro <proyects_serch@hotmail.com>
2022-03-09 21:36:15 -0600

    adding post components

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -22,12 +22,12 @@ const Post = ({image, user, time, message}) => {
         <p className="message"> {message} </p>
 
         <div className="actions">
-        {/* Font Awesome icons */}
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
-    </div>
+          {/* Font Awesome icons */}
+          <i class="far fa-comment"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="far fa-heart"></i>
+          <i class="fas fa-share"></i>
+        </div>
       </div>
 
       <i class="fas fa-ellipsis-h"></i>
--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -1,8 +1,11 @@
 import { Routes, Route, Navigate} from 'react-router-dom'
+import NabvarMovil from '../components/NavbarMovil';
 import NavbarWeb from '../components/NavbarWeb';
 import Emergencies from '../pages/Emergencies';
 
 import Feed from '../pages/Feed';
+import OnePost from '../pages/OnePost';
+import Postuser from '../pages/Postuser';
 import Profile from '../pages/Profile';
 
 export const DashboardRoutes = () => {
@@ -10,19 +13,33 @@ export const DashboardRoutes = () => {
 
     <div className='container-fluid'>
         <div className='row'>
-            <div className='col-3'>
+            <div className='d-none d-md-flex  col-md-3 container-navbar-web' >
                 <NavbarWeb/>
             </div>
-            <div className='col-6'>
+            <div className=' col-12 col-md-6'>
                 <Routes>
                     <Route path="profile" element={<Profile />} />
                     <Route path="emergencies" element={<Emergencies />} />
+                    <Route path="postuser" element={<Postuser />} />
+                    <Route path="onepost" element={<OnePost />} />
                     <Route path='/' element = {<Feed/>} />
                     <Route path = '*' element = { <Navigate replace to = '/' />} />
                 </Routes>
+                <NabvarMovil/>
             </div>
 
-            <div className='col-3'>
+            <div className='d-none d-md-flex  col-md-3'>
+                <div className="form-group">
+                    <input
+                        id = "passwordlabel"
+                        type="text"
+                        //value = {password}
+                        className="form-control formInput"
+                        //placeholder="Contraseña"
+                        name = "search"
+                        //onChange={ handleInputChange}
+                    />
+                </div>
             </div>
 
         </div>
