f82aee2c40c7cb564df0ba0915c0715556c025a3
SerchPro <proyects_serch@hotmail.com>
2022-03-10 19:22:06 -0600

    adding style to profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -10,12 +10,28 @@ const { username, url_user } = useSelector(state => state.auth)
 
   console.log(url_user)
   return (
-    <div className='container-profile'>
+    <div className='container form-Profile'>
+        <div className='row '>
+          <div className='col-12'>
+            <div className='noPadding centerImg'>
+              <Link to="/edit-profile">
+                  <img
+                      src={url_user}
+                      className="edit-profile-img"
+                      alt="profile"
+                      />
+                  <br/>
+                  <p > @{username} </p>
+                </Link>
+            </div>
+            <div className=' noPadding centerImg'>
+                <button type="button" className='btnwhite'> Mis publicaciones </button>
+                <button type="button" className='btnwhite '> <i class="fa-solid fa-heart"></i>  Favoritos </button>
+            </div>
+          </div>
+        </div>
+
         
-        <img src={url_user} className="profile"/>
-        <p> @{username} </p> <span>   <Link to="/edit-profile">editar perfil </Link> </span>
-        <button type="button" className='btnwhite'> Mis publicaciones </button>
-        <button type="button" className='btnwhite'> Favoritos </button>
     </div>
   )
 }
