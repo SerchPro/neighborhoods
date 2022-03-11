876a138cced5374cc682deb560c5f135ad397f78
SerchPro <proyects_serch@hotmail.com>
2022-03-10 19:25:52 -0600

    adding style to navbar page

--- src/components/NavbarWeb/index.js
+++ src/components/NavbarWeb/index.js
@@ -5,22 +5,33 @@ import { Link } from "react-router-dom";
 const NavbarWeb = () => {
   return (
 
-    <>
-        <div className='col-3'>
-            <ul className='ulFeed'>
-            <li>
-                <Link to="/">Inicio</Link>
-            </li>
-            <li>
-                <Link to="/profile">Perfil</Link>
-            </li>
-            <li>
-                <Link to="/emergencies">Emergencias</Link>
-            </li>
-            </ul>
+    <div className='container container-navbar-web'>
+        <div className='row noPadding'>
+            <div className='col-12 noPadding'>
+                <ul className='ulFeed'>
+                    <li>
+                        <Link className='linka' to="/">
+                            Inicio
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className='linka' to="/profile">
+                            Perfil
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className='linka' to="/emergencies">
+                            Emergencias
+                        </Link>
+                    </li>
+                </ul>
+                
+            </div>
+            <div className='col-12 noPadding'>
             <button type="submit" className="btnGreen" > Nueva publicacion </button>
+            </div>
         </div>
-    </>
+    </div>
   )
 }
 
