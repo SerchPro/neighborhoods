0c4ded4bdeb374783fd1140f2cdd04d9bd35c679
SerchPro <proyects_serch@hotmail.com>
2022-03-13 22:13:27 -0600

    improve one post

--- src/pages/OnePost/index.js
+++ src/pages/OnePost/index.js
@@ -1,48 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from "react-router-dom";
+import Post from '../../components/Post';
 
 const OnePost = () => {
 
-  const user =  {
-        name: "Thoughts of Dog®",
-        image: "https://i.imgur.com/b0EdHVV.jpg",
-        handle: "dog_feelings",
-      }
-      const time = "1h ago"
-      const message = "the human likes to say. that i live here rent free. but i would argue. this housing accommodation. is my payment. for a lifetime of love. and excellent company"
-      
+  const post = {
+          id: 4,
+          title: 'happy dog',
+          user: {
+            name: "Thoughts of Dog®",
+            image: "https://i.imgur.com/b0EdHVV.jpg",
+            handle: "dog_feelings",
+          },
+          timestamp: "1h ago",
+          message:
+            "the human likes to say. that i live here rent free. but i would argue. this housing accommodation. is my payment. for a lifetime of love. and excellent company",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwictveVl8xxMMMm4aZq9Zoy85XDKc3fRN-w&usqp=CAU"
+          }
   return (
-      <div className="post">
-          <img
-              src={user.image}
-              className="profile-post"
-              alt="profile"
-              />
-        <div className="body">
-          <div className="top">
-              <span className="user">
-                  <span className="name">{user.name} </span>
-                  <span className="handle">{user.handle}</span>
-              </span>
-              <span className="timestamp">{time}</span>
-          </div>
-          <Link className='linka' to="/onepost">
-            <p className="message"> {message} </p>
-          </Link>
-          
+    <div className=" backgroud-100vh">
 
-          <div className="actions">
-            {/* Font Awesome icons */}
-            <i className="far fa-comment"></i>
-            <i className="fas fa-retweet"></i>
-            <i className="far fa-heart"></i>
-            <i className="fas fa-share"></i>
-          </div>
-        </div>
-
-        <i className="fas fa-ellipsis-h"></i>
+      <div className='d-flex justify-content-start  title-page noPadding'>
+          <i class="fa-solid fa-chevron-left d-flex align-items-center iarrow"></i>
+          <p className='text180 d-flex align-items-center m-0'> {post.title}</p>
       </div>
+
+      <div className='container one-post'>
+        <div className='row'>
+          <div className='col-12'>
+
+          <Post {...post}/>
+
+
+              
+            
+          </div>  {/* col */}
+        </div> {/* row*/}
+      </div>
+      
+    </div>
+
 
   )
 }
