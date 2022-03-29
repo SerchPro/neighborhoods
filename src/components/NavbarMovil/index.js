85b6940a0e35b39acda9d9f2ba52581545ee0097
SerchPro <proyects_serch@hotmail.com>
2022-03-29 17:29:16 -0600

    addding navabar movil css

--- src/components/NavbarMovil/index.js
+++ src/components/NavbarMovil/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 //import PropTypes from 'prop-types'
 import { Link } from "react-router-dom";
 import {  useSelector } from 'react-redux';
+import './style.css'
 
 const NabvarMovil = () => {
     const { user } = useSelector(state => state.auth);
@@ -9,28 +10,28 @@ const NabvarMovil = () => {
     return (
         <div className=" container menuMovil d-md-none">
 
-            <div className='row  align-items-center noPadding'>
-                <div className='col-3 d-flex justify-content-center noPadding'>
+            <div className='row  align-items-center p-0'>
+                <div className='col-3 d-flex justify-content-center p-0'>
                     <Link className='linka icon-movil' to="/">
                         <i className="fa-solid fa-house"></i> 
                     </Link>
                 </div>
-                <div className='col-2 d-flex justify-content-center noPadding'>
+                <div className='col-2 d-flex justify-content-center p-0'>
                     <Link className='linka icon-movil' to="/myaddress">
                         <i className="fa-solid fa-location-dot imenu"></i>
                     </Link>
                 </div>
-                <div className='col-2 d-flex justify-content-center noPadding'>
+                <div className='col-2 d-flex justify-content-center p-0'>
                     <Link className='linka icon-movil-plus' to="/create-post">
                         <i className="fa-solid fa-circle-plus"></i>
                     </Link>
                 </div>
-                <div className='col-2 d-flex justify-content-center noPadding'>
+                <div className='col-2 d-flex justify-content-center p-0'>
                     <Link className='linka icon-movil' to="/emergencies">
                         <i className="fa-solid fa-heart-pulse"></i>
                     </Link>
                 </div>
-                <div className='col-3  noPadding'>
+                <div className='col-3  p-0'>
                     <Link to={`/profile/${user.username}`} className=' d-flex justify-content-center'>
                         <img
                             src={user.image_url}
