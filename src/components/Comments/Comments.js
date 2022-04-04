63260c07624a84654216cfd2321d044eab0bc206
SerchPro <proyects_serch@hotmail.com>
2022-04-04 18:05:37 -0500

    update index to createComment

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { startLoadReview } from '../../actions/review';
 import Comment from '../Comment/Comment';
-import CreateComment from '../CreateComment'
+import CreateComment from '../CreateComment/createComment'
 //import PropTypes from 'prop-types'
 
 const Comments = ({idPost}) => {
