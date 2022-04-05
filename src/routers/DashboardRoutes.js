7a7aeb01a39a447e57ff1d943c04264bf851fd56
SerchPro <proyects_serch@hotmail.com>
2022-04-04 19:05:49 -0500

    update navbarweb name

--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { Routes, Route, Navigate} from 'react-router-dom'
 import Nabvarmovil from '../components/NavbarMovil/Navbarmovil';
-import Navbarweb from '../components/Navbarweb/Navbarweb';
+import Navbarweb from  '../components/Navbarweb/Navbarweb';
 import Address from '../pages/Address/Address';
 import ChangePassword from '../pages/ChangePassword/ChangePassword';
 import CreatePost from '../pages/CreatePost/CreatePost';
